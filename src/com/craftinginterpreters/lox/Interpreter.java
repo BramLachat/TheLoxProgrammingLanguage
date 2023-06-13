3b9518d4233644f5de2122f15f3669d7c4694702
Bram Lachat <bla.datacare@gmail.com>
2023-06-13 22:37:40 +0200

    Chapter 7.3: Runtime Errors

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -2,6 +2,15 @@ package com.craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
 
+    public void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
@@ -9,10 +18,13 @@ public class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left - (double) right;
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left / (double) right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left * (double) right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
@@ -22,14 +34,19 @@ public class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof String && right instanceof String) {
                     return (String) left + (String) right;
                 }
-                break;
+
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left > (double) right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left >= (double) right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left < (double) right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left <= (double) right;
             case BANG_EQUAL:
                 return !isEqual(left, right);
@@ -57,13 +74,24 @@ public class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case MINUS:
-                return -(double)right;
+                checkNumberOperand(expr.operator, right);
+                return -(double) right;
             case BANG:
                 return !isTruthy(right);
         }
 
         // Unreachable.
         return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
     // Lox follows Ruby’s simple rule: false and nil are falsey, and everything else is truthy.
@@ -77,6 +105,20 @@ public class Interpreter implements Expr.Visitor<Object> {
         if (left == null && right == null) return true;
         if (left == null ) return false;
         return left.equals(right);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
     }
 
     private Object evaluate(Expr expr) {
--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,12 +10,15 @@ import java.util.List;
 
 public class Lox {
 
+    private static final Interpreter interpreter = new Interpreter();
+
     /**
      * For exit codes, I’m using the conventions defined in the UNIX “sysexits.h” header.
      * It’s the closest thing to a standard I could find.
      *  --> https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
      */
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -45,13 +48,18 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
 
-        // Indicate an error in the exit code
+        // Indicate an error in the exit code.
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
 
         Parser parser = new Parser(tokens);
         Expr expression = parser.parse();
@@ -60,6 +68,9 @@ public class Lox {
         if (hadError) return;
 
         System.out.println(new AstPrinter().print(expression));
+
+        // TODO > Question > Why is interpreter static field and scanner and parser not?
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -72,6 +83,12 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
