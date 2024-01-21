a05a430efaf41a15f07190b4a87251cc594ff3a7
Bram Lachat <bla.datacare@gmail.com>
2024-01-21 12:37:02 +0000

    Chapter 17: > 17.6: A Pratt Parser

--- src/com/craftinginterpreters/clox/compiler.c
+++ src/com/craftinginterpreters/clox/compiler.c
@@ -25,6 +25,16 @@ typedef enum {
     PREC_PRIMARY
 } Precedence;
 
+// That ParseFn type is a simple typedef for a function type that takes no arguments and returns nothing.
+// C’s syntax for function pointer types is so bad that I always hide it behind a typedef. I understand the intent behind the syntax—the whole “declaration reflects use” thing—but I think it was a failed syntactic experiment.
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -120,6 +130,11 @@ static void endCompiler() {
     emitReturn();
 }
 
+// This is what happens when you write your VM in a language that was designed to be compiled on a PDP-11.
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
 static void binary() {
     TokenType operatorType = parser.previous.type;
     ParseRule* rule = getRule(operatorType);
@@ -158,8 +173,68 @@ static void unary() {
     }
 }
 
+ParseRule rules[] = {
+    [TOKEN_LEFT_PAREN]          = {grouping,    NULL,       PREC_NONE},
+    [TOKEN_RIGHT_PAREN]         = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_LEFT_BRACE]          = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_RIGHT_BRACE]         = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_COMMA]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_DOT]                 = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_MINUS]               = {unary,       binary,     PREC_TERM},
+    [TOKEN_PLUS]                = {NULL,        binary,     PREC_TERM},
+    [TOKEN_SEMICOLON]           = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_SLASH]               = {NULL,        binary,     PREC_FACTOR},
+    [TOKEN_STAR]                = {NULL,        binary,     PREC_FACTOR},
+    [TOKEN_BANG]                = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_BANG_EQUAL]          = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_EQUAL]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_EQUAL_EQUAL]         = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_GREATER]             = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_GREATER_EQUAL]       = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_LESS]                = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_LESS_EQUAL]          = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_IDENTIFIER]          = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_STRING]              = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_NUMBER]              = {number,      NULL,       PREC_NONE},
+    [TOKEN_AND]                 = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_CLASS]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_ELSE]                = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_FALSE]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_FOR]                 = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_FUN]                 = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_IF]                  = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_NIL]                 = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_OR]                  = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_PRINT]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_RETURN]              = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_SUPER]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_THIS]                = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_TRUE]                = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_VAR]                 = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_WHILE]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_ERROR]               = {NULL,        NULL,       PREC_NONE},
+    [TOKEN_EOF]                 = {NULL,        NULL,       PREC_NONE},
+}
+
 static void parsePrecedence(Precedence precedence) {
-    
+    advance();
+    ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+    if (prefixRule == NULL) {
+        error("Expect expression.");
+        return;
+    }
+
+    prefixRule();
+
+    while (precedence <= getRule(parser.current.type)->precedence) {
+        advance();
+        ParseFn infixRule = getRule(parser.previous.type)->infix;
+        infixRule();
+    }
+}
+
+static ParseRule* getRule(TokenType type) {
+    return &rules[type];
 }
 
 static void expression() {
