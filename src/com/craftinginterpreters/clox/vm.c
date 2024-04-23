bfa9c779b27ad9c3e5b8f1c8d5750e63d588daa9
Bram Lachat <bla.datacare@gmail.com>
2024-04-23 20:04:19 +0000

    Chapter 21: Global Variables
    > 21.2 Variable Declarations

--- src/com/craftinginterpreters/clox/vm.c
+++ src/com/craftinginterpreters/clox/vm.c
@@ -61,10 +61,12 @@ static void concatenate() {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM() {
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
     freeObjects();
 }
@@ -72,6 +74,7 @@ void freeVM() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 
 // See: https://craftinginterpreters.com/a-virtual-machine.html#binary-operators
 // For the explanation of the do while loop
@@ -109,6 +112,12 @@ static InterpretResult run() {
             case OP_TRUE: push(BOOL_VAL(true)); break;
             case OP_FALSE: push(BOOL_VAL(false)); break;
             case OP_POP: pop(); break;
+            case OP_DEFINE_GLOBAL: {
+                ObjString* name = READ_STRING();
+                tableSet(&vm.globals, name, peek(0));
+                pop();
+                break;
+            }
             case OP_EQUAL: {
                 Value b = pop();
                 Value a = pop();
@@ -158,6 +167,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINARY_OP
 }
 
