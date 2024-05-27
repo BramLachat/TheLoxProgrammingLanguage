b5cd0dab66dbfa4ad7d2c85fe08df47b936574fe
Bram Lachat <bla.datacare@gmail.com>
2024-05-27 15:46:29 +0000

    Chapter 24: Calls and Functions
    > 24.3 Call Frames

--- src/com/craftinginterpreters/clox/vm.h
+++ src/com/craftinginterpreters/clox/vm.h
@@ -2,16 +2,25 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
+#include <cstdint>
 
-#define STACK_MAX 256
+#define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
+#define FRAMES_MAX 64
+
+typedef  struct {
+    ObjFunction* function;
+    uint8_t* ip;
+    Value* slots;
+} CallFrame;
 
 // Since the stack array is declared directly inline in the VM struct, we don’t need to allocate it with malloc, ... .
 // Every piece of memory allocated with something like malloc, ... is dynamic memory and is allocated on the heap.
 typedef struct {
-    Chunk* chunk;
-    uint8_t* ip;
+    CallFrame frames[FRAMES_MAX];
+    int frameCount;
     Value stack[STACK_MAX];
     Value* stackTop;
     Table globals;
